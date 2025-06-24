23a8c748fd689ada4d0364eaa197c98c0cd84e8c
viktoria-yarema <viktoria.yarema@bettorfaster.com>
2025-06-24 21:26:00 +0300

    refactor: Remove deprecated image domain from next.config.ts
    - Eliminated 'storage.googleapis.com' from the image domains configuration in next.config.ts to streamline remote image handling.

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["storage.googleapis.com"],
     remotePatterns: [
       {
         protocol: "https",
