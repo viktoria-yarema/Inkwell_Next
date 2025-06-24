cd0596402ddd94bf2365d7f5c890ac7671ef8250
viktoria-yarema <yarema1815@gmail.com>
2025-06-24 21:27:29 +0300

    refactor: Add 'storage.googleapis.com' to image domains in next.config.ts
    - Included 'storage.googleapis.com' in the image domains configuration to support remote image loading.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    domains: ["storage.googleapis.com"],
     remotePatterns: [
       {
         protocol: "https",
